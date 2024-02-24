7aa190d7ceac1d187e9fe8a7c5cb8315abac77ed
sbuDiction <sibusisonkosi0897@gmail.com>
2024-02-24 18:32:32 +0200

    added more time formating

--- utils/helpers.js
+++ utils/helpers.js
@@ -28,8 +28,8 @@ const timeConversion = (time, adjustTimeZone = false) => {
         end = moment(0).tz(timeZone).add(milliseconds2, 'milliseconds').subtract(1, 'hours');
     }
     // Format times as strings
-    const formattedTime1 = start.format('HH:mm:ss');
-    const formattedTime2 = end.format('HH:mm:ss');
+    const formattedTime1 = start.format('HH:mm:ss A');
+    const formattedTime2 = end.format('HH:mm:ss A');
 
     return { start: formattedTime1, end: formattedTime2 };
 }
